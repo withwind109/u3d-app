1f78952bfa2b89fda8b5f68e57e3a1de501ec7eb
withwind109 <tang.chao109@hotmail.com>
2024-11-27 21:55:21 +0800

    feat:park scene

---
+++ Assets/Resources/Fox/Scripts/NpcControl.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NpcControl : MonoBehaviour
+{
+    public GameObject canvas;
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("NPC创建");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            canvas.SetActive(true);
+            Debug.Log("遭遇");
+        }
+    }
+}
---
+++ Assets/Resources/Fox/Scripts/SimpleSampleCharacterControl.cs
@@ -0,0 +1,247 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Supercyan.AnimalPeopleSample
+{
+    public class SimpleSampleCharacterControl : MonoBehaviour
+    {
+        public GameObject canvas;
+        // 控制模式的枚举
+        private enum ControlMode
+        {
+            /// <summary>
+            /// Up移动角色向前，左右逐渐转动角色，Down使角色向后移动
+            /// </summary>
+            Tank,
+            /// <summary>
+            /// 角色从摄像机的视角自由移动到选定的方向
+            /// </summary>
+            Direct
+        }
+
+        // 移动速度
+        [SerializeField] private float m_moveSpeed = 2;
+        // 转向速度
+        [SerializeField] private float m_turnSpeed = 200;
+        // 跳跃力度
+        [SerializeField] private float m_jumpForce = 4;
+
+        // 动画组件
+        [SerializeField] private Animator m_animator = null;
+        // 刚体组件
+        [SerializeField] private Rigidbody m_rigidBody = null;
+
+        // 控制模式
+        [SerializeField] private ControlMode m_controlMode = ControlMode.Tank;
+
+        // 当前的垂直和水平输入
+        private float m_currentV = 0;
+        private float m_currentH = 0;
+
+        // 插值速度
+        private readonly float m_interpolation = 10;
+        // 步行比例
+        private readonly float m_walkScale = 0.33f;
+        // 后退步行比例
+        private readonly float m_backwardsWalkScale = 0.16f;
+        // 后退奔跑比例
+        private readonly float m_backwardRunScale = 0.66f;
+
+        // 是否曾经着地
+        private bool m_wasGrounded;
+        // 当前的方向
+        private Vector3 m_currentDirection = Vector3.zero;
+
+        // 跳跃时间戳和最小跳跃间隔
+        private float m_jumpTimeStamp = 0;
+        private float m_minJumpInterval = 0.25f;
+        // 跳跃输入
+        private bool m_jumpInput = false;
+
+        // 是否着地
+        private bool m_isGrounded;
+
+        // 碰撞体列表
+        private List<Collider> m_collisions = new List<Collider>();
+
+        // 初始化组件
+        private void Awake()
+        {
+            if (!m_animator) { gameObject.GetComponent<Animator>(); }
+            if (!m_rigidBody) { gameObject.GetComponent<Animator>(); }
+        }
+
+        private void Start()
+        {
+            canvas.SetActive(false);
+        }
+
+        // 碰撞进入事件
+        private void OnCollisionEnter(Collision collision)
+        {
+            ContactPoint[] contactPoints = collision.contacts;
+            for (int i = 0; i < contactPoints.Length; i++)
+            {
+                if (Vector3.Dot(contactPoints[i].normal, Vector3.up) > 0.5f)
+                {
+                    if (!m_collisions.Contains(collision.collider))
+                    {
+                        m_collisions.Add(collision.collider);
+                    }
+                    m_isGrounded = true;
+                }
+            }
+        }
+
+        // 碰撞持续事件
+        private void OnCollisionStay(Collision collision)
+        {
+            ContactPoint[] contactPoints = collision.contacts;
+            bool validSurfaceNormal = false;
+            for (int i = 0; i < contactPoints.Length; i++)
+            {
+                if (Vector3.Dot(contactPoints[i].normal, Vector3.up) > 0.5f)
+                {
+                    validSurfaceNormal = true; break;
+                }
+            }
+
+            if (validSurfaceNormal)
+            {
+                m_isGrounded = true;
+                if (!m_collisions.Contains(collision.collider))
+                {
+                    m_collisions.Add(collision.collider);
+                }
+            }
+            else
+            {
+                if (m_collisions.Contains(collision.collider))
+                {
+                    m_collisions.Remove(collision.collider);
+                }
+                if (m_collisions.Count == 0) { m_isGrounded = false; }
+            }
+        }
+
+        // 碰撞退出事件
+        private void OnCollisionExit(Collision collision)
+        {
+            if (m_collisions.Contains(collision.collider))
+            {
+                m_collisions.Remove(collision.collider);
+            }
+            if (m_collisions.Count == 0) { m_isGrounded = false; }
+        }
+
+        // 每帧更新输入状态
+        private void Update()
+        {
+            if (!m_jumpInput && Input.GetKey(KeyCode.Space))
+            {
+                m_jumpInput = true;
+            }
+        }
+
+        // 固定更新逻辑
+        private void FixedUpdate()
+        {
+            m_animator.SetBool("Grounded", m_isGrounded);
+
+            switch (m_controlMode)
+            {
+                case ControlMode.Direct:
+                    DirectUpdate();
+                    break;
+
+                case ControlMode.Tank:
+                    TankUpdate();
+                    break;
+
+                default:
+                    Debug.LogError("Unsupported state");
+                    break;
+            }
+
+            m_wasGrounded = m_isGrounded;
+            m_jumpInput = false;
+        }
+
+        // 坦克模式更新
+        private void TankUpdate()
+        {
+            float v = Input.GetAxis("Vertical");
+            float h = Input.GetAxis("Horizontal");
+
+            bool walk = Input.GetKey(KeyCode.LeftShift);
+
+            if (v < 0)
+            {
+                if (walk) { v *= m_backwardsWalkScale; }
+                else { v *= m_backwardRunScale; }
+            }
+            else if (walk)
+            {
+                v *= m_walkScale;
+            }
+
+            m_currentV = Mathf.Lerp(m_currentV, v, Time.deltaTime * m_interpolation);
+            m_currentH = Mathf.Lerp(m_currentH, h, Time.deltaTime * m_interpolation);
+
+            transform.position += transform.forward * m_currentV * m_moveSpeed * Time.deltaTime;
+            transform.Rotate(0, m_currentH * m_turnSpeed * Time.deltaTime, 0);
+
+            m_animator.SetFloat("MoveSpeed", m_currentV);
+
+            JumpingAndLanding();
+        }
+
+        // 直接控制模式更新
+        private void DirectUpdate()
+        {
+            float v = Input.GetAxis("Vertical");
+            float h = Input.GetAxis("Horizontal");
+
+            Transform camera = Camera.main.transform;
+
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                v *= m_walkScale;
+                h *= m_walkScale;
+            }
+
+            m_currentV = Mathf.Lerp(m_currentV, v, Time.deltaTime * m_interpolation);
+            m_currentH = Mathf.Lerp(m_currentH, h, Time.deltaTime * m_interpolation);
+
+            Vector3 direction = camera.forward * m_currentV + camera.right * m_currentH;
+
+            float directionLength = direction.magnitude;
+            direction.y = 0;
+            direction = direction.normalized * directionLength;
+
+            if (direction != Vector3.zero)
+            {
+                m_currentDirection = Vector3.Slerp(m_currentDirection, direction, Time.deltaTime * m_interpolation);
+
+                transform.rotation = Quaternion.LookRotation(m_currentDirection);
+                transform.position += m_currentDirection * m_moveSpeed * Time.deltaTime;
+
+                m_animator.SetFloat("MoveSpeed", direction.magnitude);
+            }
+
+            JumpingAndLanding();
+        }
+
+        // 跳跃和着陆逻辑
+        private void JumpingAndLanding()
+        {
+            bool jumpCooldownOver = (Time.time - m_jumpTimeStamp) >= m_minJumpInterval;
+
+            if (jumpCooldownOver && m_isGrounded && m_jumpInput)
+            {
+                m_jumpTimeStamp = Time.time;
+                m_rigidBody.AddForce(Vector3.up * m_jumpForce, ForceMode.Impulse);
+            }
+        }
+    }
+}
